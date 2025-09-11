e50c0857d3509a1dd7f9ae0afc1f1184612ad455
Joaquin Leutgeb <joaquin.leut@gmail.com>
2025-09-11 17:02:21 -0300

    Table viz improvement

--- reporting/dashboard_micro.py
+++ reporting/dashboard_micro.py
@@ -70,7 +70,7 @@ def highlight_nuestro_seller(row, seller_name_to_highlight: str):
     Usa un color que funciona bien tanto en modo claro como oscuro.
     """
     if row['nombre_vendedor'] == seller_name_to_highlight:
-        return ['background-color: #d4edda; color: black; font-weight: bold;'] * len(row)
+        return ['font-weight: bold;'] * len(row)
     return [''] * len(row)
 
 # -----------------------------------------------------------------------------
